407e37a067ea076bf7094c8f396acdd507397d4a
Saee Phalke <saeesphalke@gmail.com>
2025-06-19 13:56:34 -0700

    ui changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Stop Scrolling Social Media!!!</h1>
       </header>
+      <main>
+
+        <div class="card">
+          <h3>This is how much time you've spent scrolling</h3>
+          <p>1:00</p>
+        </div>
+        <br/><br/>
+        <div class="card">
+          <h3>Here's a list of things you'd rather be doing</h3>
+          <p>
+          <form>
+            <input type="checkbox" id="todo1"/>
+            <label htmlFor="todo1"> Write a Novel</label><br />
+
+            <input type="checkbox" id="todo2"/>
+            <label htmlFor="todo2"> Go Swimming</label><br />
+
+            <input type="checkbox" id="todo3"/>
+            <label htmlFor="todo3"> Go Draw Something</label><br/> <br/>
+
+            <button>Submit</button>
+          </form></p></div>
+      </main>
     </div>
   );
 }
