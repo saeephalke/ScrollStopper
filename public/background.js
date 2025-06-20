753941b0b887c79e1bb0266bd55c7f6b925c50ac
Saee Phalke <saeesphalke@gmail.com>
2025-06-19 19:52:29 -0700

    fine tuned changes

--- public/background.js
+++ public/background.js
@@ -1,10 +1,22 @@
-chrome.webNavigation.onCompleted.addListener((details) => {
-    const url = chrome.runtime.getURL("index.html");
-    chrome.tabs.update(details.tabID, { url });
-}, {
-    url: [
-    { urlMatches: "https://www.instagram.com/*" },
-    { urlMatches: "https://www.youtube.com/shorts/*" },
-    { urlMatches: "https://www.tiktok.com/*" }
-  ]
-});
+const triggeredTabs = new Set();
+const redirectURL = chrome.runtime.getURL("index.html");
+
+function handleNavigation(details) {
+  if (triggeredTabs.has(details.tabId)) return;
+
+  triggeredTabs.add(details.tabId);
+  chrome.tabs.create({ url: redirectURL });
+
+}
+
+const urlFilter = {
+  url: [
+    { urlMatches: "https://www.instagram.com/.*" },
+    { urlMatches: "https://www.youtube.com/shorts/.*" },
+    { urlMatches: "https://www.tiktok.com/.*" }
+  ],
+  frameId: 0
+};
+
+chrome.webNavigation.onCompleted.addListener(handleNavigation, urlFilter);
+chrome.webNavigation.onHistoryStateUpdated.addListener(handleNavigation, urlFilter);
