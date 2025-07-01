78e65add0bc394b1b49c9a8aac753106d9d8035a
Saee Phalke <saeesphalke@gmail.com>
2025-07-01 15:14:13 -0700

    tracks usertime

--- public/background.js
+++ public/background.js
@@ -1,18 +1,64 @@
 const triggeredTabs = new Set();
 const redirectURL = chrome.runtime.getURL("index.html");
+const scrollSites = [
+  "www.instagram.com",
+  "www.youtube.com",
+  "www.tiktok.com",
+];
+
+let activeStartTime = null; //timer
+let activeHost = null; //the current host
 
 function handleNavigation(details) {
-  if (triggeredTabs.has(details.tabId)) return;
+  if (triggeredTabs.has(details.tabId)) return; //don't trigger if tab was visited
+  
+  const url = new URL(details.url);
+  const host = url.hostname;
 
-  triggeredTabs.add(details.tabId);
-  chrome.tabs.create({ url: redirectURL });
+  if(scrollSites.includes(host)){
+    triggeredTabs.add(details.tabId); //add tab to not be triggered
+    chrome.tabs.create({ url: redirectURL }); //redirct
+    activeStartTime = Date.now();
+    activeHost = host;
+  }
 
+}
+
+function handleTabChange(details) {
+  chrome.tabs.get(details.tabId, (tab) => {
+    if(!tab.url) return;
+
+    const host = new URL(tab.url).hostname;
+
+    if(scrollSites.includes(host)) {
+      if(host != activeHost) {
+        activeStartTime = Date.now();
+        activeHost = host;
+      }
+    } else {
+      if(activeStartTime && activeHost) {
+        const duration = Date.now() - activeStartTime;
+        saveTime(activeHost, duration);
+        activeStartTime = null;
+        activeHost = null;
+      }
+    }
+  });
+}
+
+function saveTime(host, duration) {
+  chrome.storage.local.get(["siteTimes"], (result) => {
+    const siteTimes = result.siteTimes || {};
+    siteTimes[host] = (siteTimes[host] || 0) + duration;
+    chrome.storage.local.set({ siteTimes }, () => {
+    });
+  });
 }
 
 const urlFilter = {
   url: [
     { urlMatches: "https://www.instagram.com/.*" },
-    { urlMatches: "https://www.youtube.com/shorts/.*" },
+    { urlMatches: "https://www.youtube.com/.*" },
     { urlMatches: "https://www.tiktok.com/.*" }
   ],
   frameId: 0
@@ -20,3 +66,4 @@ const urlFilter = {
 
 chrome.webNavigation.onCompleted.addListener(handleNavigation, urlFilter);
 chrome.webNavigation.onHistoryStateUpdated.addListener(handleNavigation, urlFilter);
+chrome.tabs.onActivated.addListener(handleTabChange);
--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+/* global chrome */
 import { useEffect, useState } from 'react';
 import './App.css';
+import { use } from 'react';
 
 function App() {
-
+  //important states for the app
   const [userID, setUserID] = useState("");
   const [todos, setTodos] = useState([]);
   const [checkedTasks, setCheckedTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const [siteTimes, setSiteTime] = useState({});
 
+  //get the userID
   useEffect(() => {
     /* chrome.storage.local.get(['scrollStopperUserID'], async (result) => {
       let storedUserID = result.scrollStopperUserID;
@@ -23,21 +27,41 @@ function App() {
   fetchUserID();
   }, []);
 
+  //get the user's tasks
   useEffect(() => {
     const fetchTasks = async() => {
+
+      //api call to get tasks based on userID
       if(!userID) return;
       try{
         const res = await fetch(`http://localhost:3010/todos/${userID}`); 
         const data = await res.json();
         setTodos(data); 
       } catch (error) {
+        //error handling
         console.log("error catching todos");
       } 
     }
     fetchTasks();
+    //dependant on userID obviously
   }, [userID])
 
+  //gets user's time
+  useEffect(() => {
+    if(typeof chrome !== "undefined" && chrome.storage) {
+      const interval = setInterval(() => {
+        chrome.storage.local.get(["siteTimes"], (result) => {
+          setSiteTime(result.siteTimes || {}); 
+        });
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [])
+
+  //delete any unwanted tasks
   const deleteTasks = async () => {
+
+    //api call
     const res = await fetch(`http://localhost:3010/todos/`, {
       method: "DELETE",
       headers: {
@@ -45,10 +69,14 @@ function App() {
       },
       body: JSON.stringify({ids: checkedTasks})
     });
+
+    //filters out deleted tasks from the todos
     setTodos(prevTodos => prevTodos.filter(todo => !checkedTasks.includes(todo._id)));
+    //clears the todos
     setCheckedTasks([]);
   }
 
+  //add a new task
   const addTask = async () => {
     console.log(newTask);
     const res = await fetch(`http://localhost:3010/todos/`, {
@@ -63,32 +91,64 @@ function App() {
     })
 
     const added = await res.json();
-    setTodos(prev => [...prev, added]);
+    setTodos(prev => [...prev, added]); //adds to the todos to make a change
     setNewTask("");
   }
 
+  //this function is used to format time
+  const formatTime = (ms) => {
+    const totalSec = Math.floor(ms / 1000);
+    const hours = Math.floor(totalSec / 3600);
+    const minutes = Math.floor((totalSec % 3600) / 60);
+    const seconds = totalSec % 60;
+    return `${hours}h ${minutes}m ${seconds}s`;
+  }
+
+  //this function formats the host names
+  const formatHostname = (host) => {
+    const customNames = {
+      "www.instagram.com": "Instagram",
+      "www.youtube.com": "YouTube",
+      "www.tiktok.com": "TikTok",
+    };
+
+    if (customNames[host]) return customNames[host];
+  }
+
+
   return (
     <div className="App">
+
       <header>
         <h1>Stop Scrolling Social Media!!!</h1>
       </header>
       <main>
 
+
         <div class="card">
           <h3>This is how much time you've spent scrolling</h3>
-          <p>1:00</p>
+          <div>
+            {Object.entries(siteTimes).map(([host, time]) => (
+              <p key = {host}>
+                <b>{formatHostname(host)} </b> : {formatTime(time)}
+              </p>
+            ))}
+          </div>         
         </div>
+
+
         <br/><br/>
         <div class="card">
-          <h3>Here's a list of things you'd rather be doing</h3>
-          
+          <h3>Here's a list of things you'd rather be doing</h3>      
           <form>
             {todos.map((d, i) => 
               <div key={d._id}><input type="checkbox" id={i} 
               onChange={(e) => {
                 if(e.target.checked) {
+                  //adds tasks to checkedTasks when checked
                   setCheckedTasks(prev => [...prev, d._id])
                 } else {
+                  //removes tasks from checkedTasks when checked
                   setCheckedTasks(prev => prev.filter(id => id !== d._id))
                 }
               }}/>
@@ -96,6 +156,7 @@ function App() {
             )}
             <br/>
             <button onClick={(e) => {
+              //deletes checked tasks on click
               e.preventDefault();
               deleteTasks();
             }
@@ -103,20 +164,26 @@ function App() {
           </form><br/></div>
           <br/><br/>
 
+
           <div class="card">
             <h3>Add something new</h3>
             
             <form>
               <input type="text" id="newtask" placeholder="Add New Task" value={newTask}
-              onChange={(e) => setNewTask(e.target.value)}/>
+              onChange={(e) => 
+              /*changes value in textbox to be new task */ 
+              setNewTask(e.target.value)}/>
               <label htmlFor="newtask"></label>
               <br/><br/>
               <button onClick={(e) => {
+                //adds new task on click
                 e.preventDefault();
                 addTask();
               }}>Add New Wishlist Item</button>
             </form><br/>
           </div>
+
+
         <br/><br/>
       </main>
     </div>
